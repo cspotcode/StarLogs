de505c6b4066cc2883e9bde75947a59f835e51fc
Andrew Bradley <abradley@brightcove.com>
2016-01-07 00:35:31 -0500

    Updates static site

--- javascripts/guts.js
+++ javascripts/guts.js
@@ -97,7 +97,8 @@ $(function () {
     var looped = options.looped;
     if (typeof looped === 'undefined') looped = true;
 
-    var sourcePrefix = window.location.hostname == 'localhost' ? '' : 'https://dl.dropboxusercontent.com/u/362737/starlogs.net/';
+    // Can redirect to another host for the big assets
+    var sourcePrefix = '';
 
     var tag = $('<audio>', { id: fileName, loop: looped });
 
